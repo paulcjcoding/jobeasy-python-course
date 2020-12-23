f21aecd0adb9999e5c916a537756c1d8a0114b14
paulcjohnson25 <36369309+paulcjohnson25@users.noreply.github.com>
2020-12-22 19:35:29 -0600

    git init commit

--- lesson_1/homework_1_3.py
+++ lesson_1/homework_1_3.py
@@ -4,15 +4,21 @@ import math
 # Round a off to three decimal places.
 
 a = 10.04789
-result_1 = None
+result_1 = round(a, 3)
+print(result_1)
 
 
 # Round up result_2 to greater value.
 
-result_2 = 5 / 2 * 6 + 1.25 - 4
+result_2 = math.ceil(5 / 2 * 6 + 1.25 - 4)
+print(5 / 2 * 5 + 1.25 -4)
+print(result_2)
+
 
 
 # Round up result_3 to lesser value.
 
-result_3 = 8 / 3 * 5 + 4.75 - 7
+result_3 = math.floor(8 / 3 * 5 + 4.75 - 7)
+print(8 / 3 * 5 + 4.75 - 7)
+print(result_3)
 
